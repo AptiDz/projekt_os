b77f023c705cd3132c4fbabacd9d90c36379af41
Levent Kantarci <levent.kantarci@iths.se>
2024-11-24 12:37:05 +0100

    updated age swimming

--- graph_test.py
+++ graph_test.py
@@ -5,9 +5,6 @@ import hashlib as hl
 anon_df = pd.read_csv("athlete_events.csv")
 anon_df["Name"] = anon_df["Name"].apply(lambda name: hl.sha256(name.encode()).hexdigest())
 
-
-
-
 def sport_age_people(sports):
     sport = anon_df[anon_df["Sport"] == sports].reset_index(drop=True)
     sport = sport.groupby(["Age"]).size().reset_index(name="Age Count")
@@ -15,4 +12,4 @@ def sport_age_people(sports):
     
     return fig.show()
   
-sport_age_football = sport_age_people("Football")
+sport_age_swimming = sport_age_people("Swimming")
