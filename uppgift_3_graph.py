8ed4deee6c21e552d3d229f3459e80196e21ceb7
Umite93 <umit.ekici@iths.se>
2024-11-24 13:24:00 +0100

    updated uppgift_3_graph

--- uppgift_3_graph.py
+++ uppgift_3_graph.py
@@ -16,13 +16,44 @@ medals = anon_df[anon_df["Medal"].notna()].reset_index(drop=True)
 
 # Graph Medals/Sport
 
-def sport_medals(sports):
-    
-    sport = medals[medals["Sport"] == sports].reset_index(drop=True)
-    sport = sport.groupby(["NOC", "Medal"]).size().reset_index(name="Medal Count")
+sport_medals = df_gbr.groupby("Sport", observed=True)[["Medal"]].count() 
+top_10_sports = sport_medals.sort_values(by="Medal", ascending=False).head(10)
 
-    fig = px.bar(sport, x="NOC", y="Medal Count", color="Medal", color_discrete_map = {"Bronze": "tan", "Silver": "silver", "Gold": "gold"}, title="Countries with number of medals for " + sports, labels={"NOC": "Country", "Medal Count": "Number of Medals"})
+fig = px.bar(
+    top_10_sports,
+    x=top_10_sports.index,
+    y="Medal",
+    labels={"x": "Sport", "Medal": "Number of Medals"},
+    title="Number of Medals per Sport")
 
-    return fig
+medals_sport = fig
 
-sport_medals_football = sport_medals("Football")
+# Graph Medals/GBR/OS
+
+os_type_medals = df_gbr.groupby(['Games', 'Medal'], observed=True).size().unstack(fill_value=0) 
+
+os_type_medals_index = os_type_medals.reset_index()
+
+os_type_medals_melted = os_type_medals_index.melt(
+id_vars=["Games"],
+value_vars=["Bronze", "Silver", "Gold"],
+var_name="Type of Medals",
+value_name="Number of Type")
+
+fig = px.bar(
+os_type_medals_melted,
+x="Games",
+y="Number of Type",
+color="Type of Medals",
+title="Number of medals won by GBR on each OS",
+labels={"Games": "Year", "Number of Type": "Number of Medals"},
+color_discrete_map={"Bronze": "tan", "Silver": "silver", "Gold": "gold"})
+
+fig.update_layout(
+xaxis_tickangle=90,
+barmode="stack")
+
+medals_gbr_os = fig
+
+
+
