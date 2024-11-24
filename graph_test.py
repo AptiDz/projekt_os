c9ee3829344995e73838878afcbc021449d031dc
AptiDz <apti.dzhamurzaev@iths.se>
2024-11-24 11:17:06 +0100

    update grapgh

--- graph_test.py
+++ graph_test.py
@@ -1,7 +1,10 @@
 import plotly.express as px
 import pandas as pd
+import hashlib as hl
 
 anon_df = pd.read_csv("athlete_events.csv")
+anon_df["Name"] = anon_df["Name"].apply(lambda name: hl.sha256(name.encode()).hexdigest())
+
 
 sport_medals = anon_df.groupby("Sport", observed=True)[["Medal"]].count() 
 top_10_sports = sport_medals.sort_values(by="Medal", ascending=False).head(10)
