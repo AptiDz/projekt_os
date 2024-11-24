fb5248c17cb4f95fc42dcd7f9ff46916745b845b
Umite93 <umit.ekici@iths.se>
2024-11-24 11:56:52 +0100

    update graph

--- graph_test.py
+++ graph_test.py
@@ -6,14 +6,29 @@ anon_df = pd.read_csv("athlete_events.csv")
 anon_df["Name"] = anon_df["Name"].apply(lambda name: hl.sha256(name.encode()).hexdigest())
 
 
-sport_medals = anon_df.groupby("Sport", observed=True)[["Medal"]].count() 
-top_10_sports = sport_medals.sort_values(by="Medal", ascending=False).head(10)
+
+df_gbr = anon_df[anon_df["NOC"] == "GBR"]
+os_type_medals = df_gbr.groupby(['Games', 'Medal'], observed=True).size().unstack(fill_value=0) 
+
+os_type_medals_index = os_type_medals.reset_index()
+
+os_type_medals_melted = os_type_medals_index.melt(
+    id_vars=["Games"],
+    value_vars=["Bronze", "Silver", "Gold"],
+    var_name="Type of Medals",
+    value_name="Number of Type")
 
 fig = px.bar(
-        top_10_sports,
-        x=top_10_sports.index,
-        y="Medal",
-        labels={"x": "Sport", "Medal": "Number of Medals"},
-        title="Number of Medals per Sport")
+    os_type_medals_melted,
+    x="Games",
+    y="Number of Type",
+    color="Type of Medals",
+    title="Number of medals won by GBR on each OS",
+    labels={"Games": "Year", "Number of Type": "Number of Medals"},
+    color_discrete_map={"Bronze": "tan", "Silver": "silver", "Gold": "gold"})
+
+fig.update_layout(
+    xaxis_tickangle=90,
+    barmode="stack")
 
 fig.show()
