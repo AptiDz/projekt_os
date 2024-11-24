fd0184aa447be638693a83d2b91d5a2dc1602517
Levent Kantarci <levent.kantarci@iths.se>
2024-11-24 12:01:46 +0100

    update graph

--- graph_test.py
+++ graph_test.py
@@ -8,27 +8,14 @@ anon_df["Name"] = anon_df["Name"].apply(lambda name: hl.sha256(name.encode()).he
 
 
 df_gbr = anon_df[anon_df["NOC"] == "GBR"]
-os_type_medals = df_gbr.groupby(['Games', 'Medal'], observed=True).size().unstack(fill_value=0) 
 
-os_type_medals_index = os_type_medals.reset_index()
+fig = px.histogram(
+    df_gbr,
+    x="Age",
+    nbins=8,
+    title="OS-GBR: Participants Ages",
+    labels={"Age": "Age Bracket", "Count" : "Number of Individuals"})
 
-os_type_medals_melted = os_type_medals_index.melt(
-    id_vars=["Games"],
-    value_vars=["Bronze", "Silver", "Gold"],
-    var_name="Type of Medals",
-    value_name="Number of Type")
-
-fig = px.bar(
-    os_type_medals_melted,
-    x="Games",
-    y="Number of Type",
-    color="Type of Medals",
-    title="Number of medals won by GBR on each OS",
-    labels={"Games": "Year", "Number of Type": "Number of Medals"},
-    color_discrete_map={"Bronze": "tan", "Silver": "silver", "Gold": "gold"})
-
-fig.update_layout(
-    xaxis_tickangle=90,
-    barmode="stack")
+fig.update_layout(yaxis_title="Number of Individuals")
 
 fig.show()
