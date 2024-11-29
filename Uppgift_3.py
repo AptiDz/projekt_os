422e3e32d7a5cab4e90b6c2ef5a0f622f9e2dfdd
AptiDz <apti.dzhamurzaev@iths.se>
2024-11-29 12:27:34 +0100

    references and comments updated

--- Uppgift_3.py
+++ Uppgift_3.py
@@ -1,22 +1,33 @@
-import dash
-from dash import Dash, html, dcc, Input, Output
+# Imports the libraries.
+# Imports libraries, variables and functions from the file.
+from dash import Dash, html, dcc, Input, Output, ctx
 import dash_bootstrap_components as dbc
 from uppgift_3_graph import *
 
 # Intializes dash in its darkly theme
 app = Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 
-# Server to get it running in render
+# Server to get it running in render online
 server = app.server
 
 # Defines the layout
 app.layout = html.Div(
     [html.H1(
             "Great Britain in OS!", className="mb-5",
-        ),
+        ) # Creates the title for the dash app and its gets margin by 5 on bottom so the buttons have distance verticly
+        ,
+        
+        # Title for upper 3 buttons which represants Country statistics
+        html.H3("Country Statistics", className="mb-3"), 
+        
+        # Creates the buttons with specific name and id and margin me means margin ends with has distance to next element on the right side. n_clicks represents that the button is not clicked at the start.
         dbc.Button("Medals/Sport", id="button1", className="me-2 mb-5", n_clicks=0),
         dbc.Button("Medals/GBR/OS", id="button2", className="me-2 mb-5", n_clicks=0),
         dbc.Button("Participants/Ages", id="button3", className="me-2 mb-5", n_clicks=0),
+        
+         # Title for the lower 8 buttons which represants Sport statistics
+        html.H3("Sport Statistic", className="mb-3"),  # Subheading with bottom margin
+        
         dbc.Button("Medals/Football/Countries", id="button4", className="me-2 mb-5", n_clicks=0),
         dbc.Button("Medals/Swimming/Countries", id="button5", className="me-2 mb-5", n_clicks=0),
         dbc.Button("Medals/Boxing/Countries", id="button6", className="me-2 mb-5", n_clicks=0),
@@ -24,7 +35,8 @@ app.layout = html.Div(
         dbc.Button("Individuals/Age/Football", id="button8", className="me-2 mb-5", n_clicks=0),
         dbc.Button("Individuals/Age/Swimming", id="button9", className="me-2 mb-5", n_clicks=0),
         dbc.Button("Individuals/Age/Boxing", id="button10", className="me-2 mb-5", n_clicks=0),
-        dbc.Button("Individuals/Age/Rugby", id="button11", className="me-2 mb-5", n_clicks=0),
+        dbc.Button("Individuals/Age/Rugby", id="button11", className="me-2 mb-5", n_clicks=0), 
+        # Bottom margin to separate from next section,
         html.Div(
             dcc.Graph(id="graph")  # The graph updates dynamically
         ),
@@ -33,18 +45,16 @@ app.layout = html.Div(
 
 # Defining the callbacks
 @app.callback(
-    Output("graph", "figure"),  # Targets "figure" property of the graph
-    [Input(f"button{i+1}", "n_clicks") for i in range(11)]
+    Output("graph", "figure"),  # Targets "figure" property of the graph as the output
+    [Input(f"button{i+1}", "n_clicks") for i in range(11)] # Input uppdates n_clicks by pressing the specific button.
 )
 def interact_graph(*btns):
     # Determining which button was based on clicks on n_clicks
-    ctx = dash.callback_context
     if not ctx.triggered:
         return {}  # Returns the empty figure by default
-    # id = ctx.triggered returns id.n_clicks (index[0] = id, index[1] = n_clicks)
+    # id = ctx.triggered returns id.n_clicks (index[0] = id, index[1] = n_clicks). Second [0] returns the first element this case button"(n)"
     id = ctx.triggered[0]["prop_id"].split(".")[0] 
-
-    # id checks the condition of of dbc.button id
+    # id checks the condition of of dbc.button id and returns the specific values in this case the variables that plots the plotly
     if id == "button1":
         return medals_sport
 
@@ -81,7 +91,7 @@ def interact_graph(*btns):
     else:
         return {}  # At the start of the dash without clicking button results empty figure
 
-# Runs the dash
+# Runs the dash with debug features on which provides detailed error messages.
 if __name__ == "__main__":
     app.run_server(debug=True)
     
@@ -91,6 +101,8 @@ Källförteckning
 
 https://github.com/pr0fez/AI24-Databehandling 
 https://dashcheatsheet.pythonanywhere.com/
-https://dash-bootstrap-components.opensource.faculty.ai
+https://dash-bootstrap-components.opensource.faculty.ai/docs/components/button/
+https://dash.plotly.com/advanced-callbacks
+
 
 """
